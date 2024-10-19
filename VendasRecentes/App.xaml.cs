7c96b0795b61cca5cd3a5fcbae29f2606b032922
caio <caionasc@gmail.com>
2024-10-18 22:49:37 -0300

    Adicionar arquivos de projeto.

---
+++ VendasRecentes/App.xaml.cs
@@ -0,0 +1,14 @@
+﻿using System.Configuration;
+using System.Data;
+using System.Windows;
+
+namespace VendasRecentes
+{
+    /// <summary>
+    /// Interaction logic for App.xaml
+    /// </summary>
+    public partial class App : Application
+    {
+    }
+
+}
